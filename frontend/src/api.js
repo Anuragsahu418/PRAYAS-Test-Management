30d28e55c897186bfd1d31203901347630813b50
Anuragsahu418 <anuragsahu2004125@gmail.com>
2026-08-09 18:11:03 +0530

    Fix black screen and restore API proxy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? "https://prayas-charitable-trust.vercel.app"
-    : "http://localhost:5000",
+  baseURL: "/api",
 });
 
 export default api;
